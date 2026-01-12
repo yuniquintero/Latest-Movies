80454f5a04f6afb871de193cbc71e687f4759d5b
yuniquintero <yuni.quintero@gmail.com>
2026-01-12 11:49:21 +0100

    Initial Commit

---
+++ Latest-Movies/Latest_MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Latest_MoviesApp.swift
+//  Latest-Movies
+//
+//  Created by Yuni Quintero on 12/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Latest_MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
